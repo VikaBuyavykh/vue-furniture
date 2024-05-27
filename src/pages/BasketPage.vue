2f4f412d174a25b3a79f9cb4e470591cf9c3c7c8
Vika Buyavykh <vikabuyavykh@gmail.com>
2024-05-27 13:40:30 +0300

    paths

--- src/pages/BasketPage.vue
+++ src/pages/BasketPage.vue
@@ -9,7 +9,7 @@ const basket = [
     name: 'Graystone vase',
     desc: 'A timeless ceramic vase with a tri color grey glaze.',
     price: '£85',
-    img: '/src/images/products/vase.jpg',
+    img: '@/images/products/vase.jpg',
     alt: 'The picture of the textured light grey vase',
     amount: 1
   },
@@ -17,7 +17,7 @@ const basket = [
     name: 'Basic white vase',
     desc: 'Beautiful and simple this is one for the classics collections',
     price: '£85',
-    img: '/src/images/products/vase2.jpg',
+    img: '@/images/products/vase2.jpg',
     alt: 'The picture of the white vase',
     amount: 1
   }
